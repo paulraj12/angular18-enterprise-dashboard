c3de74fd4e5116cad7a302d0112a3a5238b30e60
Paulraj <paulrajc.indiaka@gmail.com>
2026-02-13 13:06:17 +0530

    cleaned code

--- src/app/features/dashboard/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard/dashboard.component.ts
@@ -23,17 +23,3 @@ export class DashboardComponent {
   ];
 }
 
-
-// import { Component } from '@angular/core';
-// import { authStore } from '../../../core/auth/auth.store';
-
-// @Component({
-//   selector: 'app-dashboard',
-//   standalone: true,
-//   imports: [],
-//   templateUrl: './dashboard.component.html',
-//   styleUrl: './dashboard.component.scss'
-// })
-// export class DashboardComponent {
-// user = authStore.user;
-// }
--- src/app/features/users/users/users.component.ts
+++ src/app/features/users/users/users.component.ts
@@ -50,30 +50,3 @@ export class UsersComponent {
   // }
 }
 
-
-// import { HttpClient } from '@angular/common/http';
-// import { Component, effect } from '@angular/core';
-// import {usersUrl, search, page} from './users.store';
-
-// @Component({
-//   selector: 'app-users',
-//   standalone: true,
-//   imports: [],
-//   templateUrl: './users.component.html',
-//   styleUrl: './users.component.scss'
-// })
-// export class UsersComponent {
-//   users: any[] = [];
-
-//   constructor(private http: HttpClient) {
-//     effect(() => {
-//       this.http.get<any>(usersUrl()).subscribe(res => {
-//         this.users = res.users;
-//       });
-//     });
-//   }
-
-//   onSearch(e: any) {
-//     search.set(e.target.value);
-//   }
-// }
